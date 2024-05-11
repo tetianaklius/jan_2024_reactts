cdf41ee87f08426605f8a195540581d656a8bed7
tetianaklius <tetyanaklyus@gmail.com>
2024-05-11 14:56:41 +0300

    hw 3

--- src/components/StorageComponent.tsx
+++ src/components/StorageComponent.tsx
@@ -1,13 +1,15 @@
 import React, {FC} from 'react';
 import {useStorage} from "../hooks/useStorage";
 
+
 const StorageComponent: FC = () => {
     let someData = {name: "Hanna", age: 35}
     useStorage("user", localStorage, someData);
 
-    let valueToShow = localStorage.getItem("user") || "{}";
-    if (valueToShow !== null) {
-        valueToShow = JSON.parse(valueToShow);
+    let value = localStorage.getItem("user");
+    let valueToShow;
+    if (value !== null) {
+        valueToShow = JSON.parse(value);
     }
 
     return (
