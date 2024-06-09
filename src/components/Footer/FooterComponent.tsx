d6a0f2c239e5c67042d7714e498c730619faca1e
tetianaklius <tetyanaklyus@gmail.com>
2024-06-09 16:53:26 +0300

    hw 12 added footer

---
+++ src/components/Footer/FooterComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from "../Footer/FooterComponent.module.css"
+
+export const FooterComponent = () => {
+    return (
+        <div className={styles.footer}>
+            Footer
+        </div>
+    );
+};
